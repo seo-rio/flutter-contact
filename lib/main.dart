96d47fddb548275b1bbe618ae7f68e5d57e71f77
seo-rio <scs9310@gmail.com>
2022-03-22 21:32:24 +0900

    Typography, Layout 적용

--- lib/main.dart
+++ lib/main.dart
@@ -31,18 +31,35 @@ class MyApp extends StatelessWidget {
       //   ),
       // )
       home: Scaffold(
-        appBar: AppBar(title: Text('THIS IS APP')),
-        // Align 옵션으로 원하는 위치에 정렬 시킬 수 있음
-        body: Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            width: 150, height: 50, color: Colors.blue,
-            // Padding, Margin 같은걸 제외한 다른 옵션을 설정할땐 Decoration 사용
-            // decoration: BoxDecoration(
-            //   border: Border.all(color: Colors.black)
-            // ),
-          ),
+        // Title: 제목
+        // leading: 왼쪽 아이콘
+        // actions: 오른쪽 아이콘 목록
+        appBar: AppBar(actions: const [Icon(Icons.star), Icon(Icons.star)],leading: Icon(Icons.star), title: Text('THIS IS APP')),
+        body: SizedBox(
+          // 스타일링 가능한 버튼
+          // child: ElevatedButton(child: Text('글자'), onPressed: (){},),
+          // Icon 버튼
+          // child: IconButton(icon: Icon(Icons.star), onPressed: (){},),
+          // child: Text('Hello',
+          //     // 일반 컬러 설정
+          //     // style: TextStyle(color: Colors.red),
+          //     // Hex 컬러 설정
+          //     // style: TextStyle(color: Color(0x000f3123)),
+          //     // RGB 컬러 설정
+          //     // style: TextStyle(color: Color.fromRGBO(231, 48, 48, 1.0)),
+          // ),
         ),
+        // Align 옵션으로 원하는 위치에 정렬 시킬 수 있음
+        // body: Align(
+        //   alignment: Alignment.bottomCenter,
+        //   child: Container(
+        //     width: 150, height: 50, color: Colors.blue,
+        //     // Padding, Margin 같은걸 제외한 다른 옵션을 설정할땐 Decoration 사용
+        //     // decoration: BoxDecoration(
+        //     //   border: Border.all(color: Colors.black)
+        //     // ),
+        //   ),
+        // ),
       ),
     );
   }
